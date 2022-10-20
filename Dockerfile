0e601edb1eab96512fd23ae5c029cafae098d679
Lipatov Alexander <lipatov.work@bk.ru>
2022-10-21 00:04:44 +0300

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,26 @@
+﻿# Set the base image as the .NET 6.0 SDK (this includes the runtime)
+FROM mcr.microsoft.com/dotnet/sdk:6.0 as build-env
+
+# Copy everything and publish the release (publish implicitly restores and builds)
+WORKDIR /app
+COPY . ./
+RUN dotnet publish ./LambdaUpdateAliasAction/LambdaUpdateAliasAction.csproj -c Release -o out --no-self-contained
+
+# Label the container
+LABEL maintainer="Lipatov Alexander <lipatov.work@bk.ru>"
+LABEL repository="https://github.com/LipatovAlexander/LambdaUpdateAliasAction"
+LABEL homepage="https://github.com/LipatovAlexander/LambdaUpdateAliasAction"
+
+# Label as GitHub action
+LABEL com.github.actions.name="Lambda update alias action"
+# Limit to 160 characters
+LABEL com.github.actions.description="A Github action that updates a AWS lambda alias"
+# See branding:
+# https://docs.github.com/actions/creating-actions/metadata-syntax-for-github-actions#branding
+LABEL com.github.actions.icon="activity"
+LABEL com.github.actions.color="orange"
+
+# Relayer the .NET SDK, anew with the build output
+FROM mcr.microsoft.com/dotnet/sdk:6.0
+COPY --from=build-env /app/out .
+ENTRYPOINT [ "dotnet", "/LambdaUpdateAliasAction.dll" ]
---
+++ LambdaUpdateAliasAction/Program.cs
@@ -0,0 +1,39 @@
+﻿using CommandLine;
+using LambdaUpdateAliasAction;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using static CommandLine.Parser;
+
+using var host = Host.CreateDefaultBuilder(args)
+    .Build();
+    
+static TService Get<TService>(IHost host)
+    where TService : notnull =>
+    host.Services.GetRequiredService<TService>();
+
+var parser = Default.ParseArguments(() => new ActionInputs(), args);
+parser.WithNotParsed(
+    errors =>
+    {
+        Get<ILoggerFactory>(host)
+            .CreateLogger(nameof(Program))
+            .LogError("Errors occurred while parsing inputs: {Errors}", 
+            string.Join(Environment.NewLine, errors.Select(error => error.ToString())));
+        
+        Environment.Exit(2);
+    });
+    
+await parser.WithParsedAsync(inputs => UpdateAliasAsync(inputs, host));
+await host.RunAsync();
+
+static Task UpdateAliasAsync(ActionInputs inputs, IHost host)
+{
+    var logger = Get<ILoggerFactory>(host).CreateLogger(nameof(UpdateAliasAsync));
+    
+    logger.LogInformation("Hello world!");
+
+    Environment.Exit(0);
+    
+    return Task.CompletedTask;
+}
