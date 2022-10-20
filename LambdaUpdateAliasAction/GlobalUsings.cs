d1d787106bc1e4eaa80e0d29290d0d316416aac6
Lipatov Alexander <lipatov.work@bk.ru>
2022-10-21 01:26:00 +0300

    feat: Get latest version of the function

---
+++ LambdaUpdateAliasAction/GlobalUsings.cs
@@ -0,0 +1,8 @@
+global using Amazon;
+global using Amazon.Lambda;
+global using Amazon.Lambda.Model;
+global using Amazon.Runtime;
+global using CommandLine;
+global using LambdaUpdateAliasAction;
+global using static CommandLine.Parser;
+global using Environment = System.Environment;
--- LambdaUpdateAliasAction/Program.cs
+++ LambdaUpdateAliasAction/Program.cs
@@ -1,39 +1,58 @@
-﻿using CommandLine;
-using LambdaUpdateAliasAction;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using static CommandLine.Parser;
-
-using var host = Host.CreateDefaultBuilder(args)
-    .Build();
-    
-static TService Get<TService>(IHost host)
-    where TService : notnull =>
-    host.Services.GetRequiredService<TService>();
-
-var parser = Default.ParseArguments<ActionInputs>(args);
+﻿var parser = Default.ParseArguments<ActionInputs>(args);
 parser.WithNotParsed(
     errors =>
     {
-        Get<ILoggerFactory>(host)
-            .CreateLogger(nameof(Program))
-            .LogError("Errors occurred while parsing inputs: {Errors}", 
+        Console.Error.WriteLine("Errors occurred while parsing inputs: {0}", 
             string.Join(Environment.NewLine, errors.Select(error => error.ToString())));
         
         Environment.Exit(2);
     });
-    
-await parser.WithParsedAsync(inputs => UpdateAliasAsync(inputs, host));
-await host.RunAsync();
 
-static Task UpdateAliasAsync(ActionInputs inputs, IHost host)
+await parser.WithParsedAsync(UpdateAliasAsync);
+
+static async Task UpdateAliasAsync(ActionInputs inputs)
 {
-    var logger = Get<ILoggerFactory>(host).CreateLogger(nameof(UpdateAliasAsync));
+    var lambda = CreateLambdaClient(inputs);
+    var latestVersion = await FindLatestVersionAsync(lambda, inputs);
     
-    logger.LogInformation("Hello world!");
+    Console.WriteLine($"::set-output name=latest-version::{latestVersion}");
 
     Environment.Exit(0);
+}
+
+static IAmazonLambda CreateLambdaClient(ActionInputs inputs)
+{
+    var credentials = new BasicAWSCredentials(inputs.AwsAccessKeyId, inputs.AwsSecretAccessKey);
+    var region = RegionEndpoint.GetBySystemName(inputs.AwsRegion);
     
-    return Task.CompletedTask;
+    return new AmazonLambdaClient(credentials, region);
+}
+
+static async Task<string> FindLatestVersionAsync(IAmazonLambda lambda, ActionInputs inputs)
+{
+    var data = await lambda.ListVersionsByFunctionAsync(new ListVersionsByFunctionRequest
+    {
+        FunctionName = inputs.FunctionName
+    });
+
+    if (data.Versions.Count == 0)
+    {
+        Console.Error.WriteLine("No versions found");
+        Environment.Exit(3);
+    }
+
+    if (inputs.FunctionVersion is not null && data.Versions.Any(v => v.Version == inputs.FunctionVersion))
+    {
+        return inputs.FunctionVersion;
+    }
+
+    data.Versions.Sort((a, b) =>
+    {
+        int GetVersion(FunctionConfiguration fc) => fc.Version == "$LATEST" ? 0 : int.Parse(fc.Version);
+        var aVersion = GetVersion(a);
+        var bVersion = GetVersion(b);
+        return bVersion - aVersion;
+    });
+
+    return data.Versions[0].Version;
 }
