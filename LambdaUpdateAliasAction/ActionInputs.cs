2af44c5df717b22ba20f35dec080038af4371d16
Lipatov Alexander <lipatov.work@bk.ru>
2022-10-21 02:41:04 +0300

    style: Code style

--- LambdaUpdateAliasAction/ActionInputs.cs
+++ LambdaUpdateAliasAction/ActionInputs.cs
@@ -1,25 +1,23 @@
-﻿using CommandLine;
-
-namespace LambdaUpdateAliasAction;
+﻿namespace LambdaUpdateAliasAction;
 
 // ReSharper disable once ClassNeverInstantiated.Global
 public sealed class ActionInputs
 {
-    [Option("aws_access_key_id", Required = true)]
-    public string AwsAccessKeyId { get; set; } = default!;
+	[Option("aws_access_key_id", Required = true)]
+	public string AwsAccessKeyId { get; set; } = default!;
 
-    [Option("aws_secret_access_key", Required = true)]
-    public string AwsSecretAccessKey { get; set; } = default!;
+	[Option("aws_secret_access_key", Required = true)]
+	public string AwsSecretAccessKey { get; set; } = default!;
 
-    [Option("aws_region", Required = true)]
-    public string AwsRegion { get; set; } = default!;
+	[Option("aws_region", Required = true)]
+	public string AwsRegion { get; set; } = default!;
 
-    [Option("function_name", Required = true)]
-    public string FunctionName { get; set; } = default!;
+	[Option("function_name", Required = true)]
+	public string FunctionName { get; set; } = default!;
 
-    [Option("alias_name", Required = true)]
-    public string AliasName { get; set; } = default!;
+	[Option("alias_name", Required = true)]
+	public string AliasName { get; set; } = default!;
 
-    [Option("function_version", Required = false)]
-    public string? FunctionVersion { get; set; } = default!;
+	[Option("function_version", Required = false)]
+	public string? FunctionVersion { get; set; }
 }
--- LambdaUpdateAliasAction/Program.cs
+++ LambdaUpdateAliasAction/Program.cs
@@ -1,100 +1,98 @@
 ﻿var parser = Default.ParseArguments<ActionInputs>(args);
 parser.WithNotParsed(
-    errors =>
-    {
-        Console.Error.WriteLine("Errors occurred while parsing inputs: {0}", 
-            string.Join(Environment.NewLine, errors.Select(error => error.ToString())));
-        
-        Environment.Exit(1);
-    });
+	errors =>
+	{
+		Console.Error.WriteLine("Errors occurred while parsing inputs: {0}",
+			string.Join(Environment.NewLine, errors.Select(error => error.ToString())));
+
+		Environment.Exit(1);
+	});
 
 await parser.WithParsedAsync(RunAction);
 
 static async Task RunAction(ActionInputs inputs)
 {
-    var lambda = CreateLambdaClient(inputs);
-    inputs.FunctionVersion = await FindLatestVersionAsync(lambda, inputs);
-    await UpdateOrCreateAliasAsync(lambda, inputs);
+	var lambda = CreateLambdaClient(inputs);
+	inputs.FunctionVersion = await FindLatestVersionAsync(lambda, inputs);
+	await UpdateOrCreateAliasAsync(lambda, inputs);
 
-    Environment.Exit(0);
+	Environment.Exit(0);
 }
 
 static IAmazonLambda CreateLambdaClient(ActionInputs inputs)
 {
-    var credentials = new BasicAWSCredentials(inputs.AwsAccessKeyId, inputs.AwsSecretAccessKey);
-    var region = RegionEndpoint.GetBySystemName(inputs.AwsRegion);
-    
-    return new AmazonLambdaClient(credentials, region);
+	var credentials = new BasicAWSCredentials(inputs.AwsAccessKeyId, inputs.AwsSecretAccessKey);
+	var region = RegionEndpoint.GetBySystemName(inputs.AwsRegion);
+
+	return new AmazonLambdaClient(credentials, region);
 }
 
 static async Task<string> FindLatestVersionAsync(IAmazonLambda lambda, ActionInputs inputs)
 {
-    var paginator = lambda.Paginators.ListVersionsByFunction(new ListVersionsByFunctionRequest
-    {
-        FunctionName = inputs.FunctionName
-    });
+	var paginator = lambda.Paginators.ListVersionsByFunction(new ListVersionsByFunctionRequest
+	{
+		FunctionName = inputs.FunctionName
+	});
 
-    var versions = new List<FunctionConfiguration>();
+	var versions = new List<FunctionConfiguration>();
 
-    try
-    {
-        await foreach (var version in paginator.Versions)
-        {
-            versions.Add(version);
-        }
-    }
-    catch (ResourceNotFoundException)
-    {
-        Console.Error.WriteLine("Function with name: {0} does not exist", inputs.FunctionVersion);
-        Environment.Exit(3);
-    }
+	try
+	{
+		await foreach (var version in paginator.Versions) versions.Add(version);
+	}
+	catch (ResourceNotFoundException)
+	{
+		Console.Error.WriteLine("Function with name: {0} does not exist", inputs.FunctionVersion);
+		Environment.Exit(3);
+	}
 
-    if (versions.Count == 0)
-    {
-        Console.Error.WriteLine("No versions found");
-        Environment.Exit(2);
-    }
+	if (versions.Count == 0)
+	{
+		Console.Error.WriteLine("No versions found");
+		Environment.Exit(2);
+	}
 
-    if (inputs.FunctionVersion is not null)
-    {
-        if (versions.Any(v => v.Version == inputs.FunctionVersion))
-        {
-            return inputs.FunctionVersion;
-        }
+	if (inputs.FunctionVersion is not null)
+	{
+		if (versions.Any(v => v.Version == inputs.FunctionVersion)) return inputs.FunctionVersion;
 
-        Console.Error.WriteLine("Version `{0}` does not exist", inputs.FunctionVersion);
-        Environment.Exit(3);
-    }
+		Console.Error.WriteLine("Version `{0}` does not exist", inputs.FunctionVersion);
+		Environment.Exit(3);
+	}
 
-    versions.Sort((a, b) =>
-    {
-        int GetVersion(FunctionConfiguration fc) => fc.Version == "$LATEST" ? 0 : int.Parse(fc.Version);
-        var aVersion = GetVersion(a);
-        var bVersion = GetVersion(b);
-        return bVersion - aVersion;
-    });
+	versions.Sort((a, b) =>
+	{
+		static int GetVersion(FunctionConfiguration fc)
+		{
+			return fc.Version == "$LATEST" ? 0 : int.Parse(fc.Version);
+		}
 
-    return versions[0].Version;
+		var aVersion = GetVersion(a);
+		var bVersion = GetVersion(b);
+		return bVersion - aVersion;
+	});
+
+	return versions[0].Version;
 }
 
 static async Task UpdateOrCreateAliasAsync(IAmazonLambda lambda, ActionInputs inputs)
 {
-    try
-    {
-        await lambda.UpdateAliasAsync(new UpdateAliasRequest
-        {
-            FunctionName = inputs.FunctionName,
-            Name = inputs.AliasName,
-            FunctionVersion = inputs.FunctionVersion
-        });
-    }
-    catch (ResourceNotFoundException)
-    {
-        await lambda.CreateAliasAsync(new CreateAliasRequest
-        {
-            FunctionName = inputs.FunctionName,
-            Name = inputs.AliasName,
-            FunctionVersion = inputs.FunctionVersion
-        });
-    }
+	try
+	{
+		await lambda.UpdateAliasAsync(new UpdateAliasRequest
+		{
+			FunctionName = inputs.FunctionName,
+			Name = inputs.AliasName,
+			FunctionVersion = inputs.FunctionVersion
+		});
+	}
+	catch (ResourceNotFoundException)
+	{
+		await lambda.CreateAliasAsync(new CreateAliasRequest
+		{
+			FunctionName = inputs.FunctionName,
+			Name = inputs.AliasName,
+			FunctionVersion = inputs.FunctionVersion
+		});
+	}
 }
